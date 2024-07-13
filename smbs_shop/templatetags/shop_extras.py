63d705116a2b7e0d59ee5c13572f5fd7e54efcfa
Ruben Quinones <ruben@cypressbit.com>
2024-07-12 22:48:24 -0500

    Added template tags

---
+++ smbs_shop/templatetags/shop_extras.py
@@ -0,0 +1,14 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter
+def multiply(value, arg):
+    """Multiplies the value by the argument."""
+    try:
+        return value * arg
+    except (ValueError, TypeError):
+        return None
+
