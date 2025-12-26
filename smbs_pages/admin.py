0de3e57b7ed0e2eefbabd50c12ecb4489d49042c
cypressbit <ruben@cypressbit.com>
2025-12-26 01:54:25 -0600

    Update admin to add more media fields

--- smbs_pages/admin.py
+++ smbs_pages/admin.py
@@ -1,6 +1,28 @@
+import mimetypes
+import os
+
 from django.contrib import admin
+from django.utils.html import format_html
 
 from smbs_apps.smbs_pages.models import Page, Column, Row, Container, Widget, Media, Template
+
+
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ("id", "media_type", "filename", "file_url_display", "name")
+    list_filter = ("media_type",)
+    search_fields = ("name", "file")
+
+    def filename(self, obj: Media) -> str:
+        return os.path.basename(obj.file.name) if obj.file else ""
+    filename.short_description = "Filename"
+
+    def file_url_display(self, obj: Media) -> str:
+        if not obj.file:
+            return ""
+        url = obj.file_url
+        # show URL and make it clickable
+        return format_html('<a href="{}" target="_blank">{}</a>', url, url)
+    file_url_display.short_description = "File URL"
 
 
 class PageAdmin(admin.ModelAdmin):
